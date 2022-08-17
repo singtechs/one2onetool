cfd4e0e7742198b1aceb1961fbaca050b446d853
Gopala Pillai <b.gopala.pillai@accenture.com>
2022-08-18 00:35:15 +0800

    added changes

--- deploy_ec2.sh
+++ deploy_ec2.sh
@@ -7,9 +7,9 @@ docker rmi -f $(docker images -aq)
 # run the conatiner
 #docker container run -dt -p 3000:3000 --name one2onetool singtechs/one2onetool:${IMAGE_VERSION}
 if [ ${1} = "staging" ]; then
-    docker container run -dt -p 3000:3000 --name one2onetool singtechs/one2onetool:${IMAGE_VERSION}
-else
     docker container run -dt -p 3000:3000 -e DATA_FILE=Questions-test.json --name one2onetool singtechs/one2onetool:${IMAGE_VERSION}
+else
+    docker container run -dt -p 3000:3000 --name one2onetool singtechs/one2onetool:${IMAGE_VERSION}
 fi
 # check the running container
 docker ps
