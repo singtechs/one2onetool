6dd402179a596db1246eb6309d907a963157536d
singtechs <30332964+singtechs@users.noreply.github.com>
2022-08-17 13:40:56 +0800

    Update deploy_ecs.sh

--- deploy_ecs.sh
+++ deploy_ecs.sh
@@ -11,8 +11,8 @@ aws ecs register-task-definition --family ${TASK_FAMILY} --cli-input-json file:/
 # Update the service with the new task definition and desired count
 TASK_REVISION=`aws ecs describe-task-definition --task-definition ${TASK_FAMILY} | egrep "revision" | tr "/" " " | awk '{print $2}' | sed 's/"$//'`
 DESIRED_COUNT=`aws ecs describe-services --services ${SERVICE_NAME} --cluster ${CLUSTER_NAME} | egrep -m 1 "desiredCount" | tr "/" " " | awk '{print $2}' | sed 's/,$//'`
-if [ ${DESIRED_COUNT} = "0" ]; then
-    DESIRED_COUNT="1"
-fi
+#if [ ${DESIRED_COUNT} = "0" ]; then
+#    DESIRED_COUNT="1"
+#fi
 
-aws ecs update-service --cluster one2onetool-cluster --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count ${DESIRED_COUNT}
+aws ecs update-service --cluster one2onetool-cluster --service ${SERVICE_NAME} --task-definition ${TASK_FAMILY}:${TASK_REVISION} --desired-count 1
