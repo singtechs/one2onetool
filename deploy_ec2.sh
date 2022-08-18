3157c46e299f6e71636586b431c9158037f3eb7f
Gopala Pillai <b.gopala.pillai@accenture.com>
2022-08-18 13:43:11 +0800

    chnages added

--- deploy_ec2.sh
+++ deploy_ec2.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 IMAGE_VERSION="v_"${BUILD_NUMBER}
 REMOTE_USER="ec2-user"
-REMOTE_HOST="3.144.71.224"
+#REMOTE_HOST="3.144.71.224"
+REMOTE_HOST="ec2-3-144-71-224.us-east-2.compute.amazonaws.com"
 
 #sh 'scp deploy.sh ${REMOTE_USER}@${REMOTE_HOST}:~/'
 #sh 'ssh ${REMOTE_USER}@${REMOTE_HOST} "chmod +x deploy.sh"'
-sh 'ssh ${REMOTE_USER}@${REMOTE_HOST}'
+ssh ${REMOTE_USER}@${REMOTE_HOST}
 
 # Clean old images 
 docker rm -vf $(docker ps -aq)
 docker rmi -f $(docker images -aq)
 # run the conatiner
 #docker container run -dt -p 3000:3000 --name one2onetool singtechs/one2onetool:${IMAGE_VERSION}
-if [ ${1} = "staging" ]; then
+if [ ${1} = "staging" ]; then   
     docker container run -dt -p 3000:3000 -e DATA_FILE=Questions-test.json --name one2onetool singtechs/one2onetool:v_s${IMAGE_VERSION}    
 else
     docker container run -dt -p 3000:3000 --name one2onetool singtechs/one2onetool:${IMAGE_VERSION}
